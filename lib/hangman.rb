e31696a5743679319b26ff32002a37d85b93a774
samroodAli <devsamrood@gmail.com>
2021-03-22 19:13:07 +0530

    methods: getters and already attempted method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,4 +11,20 @@ class Hangman
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
 end
