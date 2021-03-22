d46b608adfebf7b225c05aeeafb311a982c42f42
samroodAli <devsamrood@gmail.com>
2021-03-22 19:09:21 +0530

    Hangman class constructor initialize function with starting game state

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,5 +5,10 @@ class Hangman
     DICTIONARY.sample
   end
 
-
+  def initialize()
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length) {"_"}
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
 end
